0223bc5404859a560c02bc9de9d7cda094e77f11
Rahul Khimsuriya <rahulkhimsuriya38@gmail.com>
2020-11-06 22:17:21 +0530

    install tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <img alt="Vue logo" src="./assets/logo.png" class="mx-auto" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     HelloWorld
   }
-};
+}
 </script>
 
 <style>
+@import './assets/css/tailwind.css';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
+  },
+  purge: {
+    content: [`./public/**/*.html`, `./src/**/*.vue`]
+  },
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
