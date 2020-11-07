bedb5d5573ef2960d2b9508d8d8bd9388922f433
Rahul Khimsuriya <rahulkhimsuriya38@gmail.com>
2020-11-07 12:19:03 +0530

    create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" class="mx-auto" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="bg-white container px-4 mx-auto">
+    <Navbar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from '@/components/Navbar'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
 
 <style>
+/* Tailwind CSS */
 @import './assets/css/tailwind.css';
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav class="py-4 flex items-center justify-between">
+    <div>
+      <img class="h-8 w-auto" :src="logo" alt="laslesVPN Logo" />
+    </div>
+
+    <ul class="flex items-center space-x-8">
+      <li
+        class="text-sm text-gray-600 font-normal tracking-wide hover:text-red-600 cursor-pointer"
+      >
+        About
+      </li>
+      <li
+        class="text-sm text-gray-600 font-normal tracking-wide hover:text-red-600 cursor-pointer"
+      >
+        Features
+      </li>
+      <li
+        class="text-sm text-gray-600 font-normal tracking-wide hover:text-red-600 cursor-pointer"
+      >
+        Pricing
+      </li>
+      <li
+        class="text-sm text-gray-600 font-normal tracking-wide hover:text-red-600 cursor-pointer"
+      >
+        Testimonials
+      </li>
+      <li
+        class="text-sm text-gray-600 font-normal tracking-wide hover:text-red-600 cursor-pointer"
+      >
+        Help
+      </li>
+    </ul>
+
+    <div class="flex space-x-6">
+      <button
+        class="text-sm leading-normal tracking-wide font-semibold text-gray-800 hover:text-red-600 transition duration-500 ease-in-out"
+      >
+        Sign In
+      </button>
+      <button
+        class="px-6 py-2 text-sm leading-normal tracking-wide font-semibold text-red-600 border border-red-600 hover:bg-red-600 hover:text-white transition duration-500 ease-in-out rounded-full"
+      >
+        Sign Up
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+import logo from '@/assets/logo.svg'
+
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      logo: logo
+    }
+  }
+}
+</script>
+
+<style scoped></style>
